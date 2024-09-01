e5f2f64fd1eb7abb2c2b449f062292bb8222d8e2
Ashwin Baskaran <ashwin.softdev@gmail.com>
2024-09-01 20:52:38 +0530

    Added mockdata temporarily and created custom hook for fetching the data.

---
+++ app/hooks/useCampaignData.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export function useCampaignData() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("/api/campaigns");
+        setData(response.data);
+        setLoading(false);
+      } catch (err) {
+        setError(err);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { data, loading, error };
+}
